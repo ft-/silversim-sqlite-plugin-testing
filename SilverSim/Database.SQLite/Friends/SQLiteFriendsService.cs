3f415c36ec6875ac1f47a4dd6c75b7d75afb3221
ft <ft@noemail>
2017-11-11 11:43:52 +0100

    corrected friends table retrieval

--- SilverSim/Database.SQLite/Friends/SQLiteFriendsService.cs
+++ SilverSim/Database.SQLite/Friends/SQLiteFriendsService.cs
@@ -41,8 +41,8 @@ namespace SilverSim.Database.SQLite.Friends
         public static FriendInfo ToFriendInfo(this SQLiteDataReader reader)
         {
             var fi = new FriendInfo();
-            fi.User.ID = reader.GetUUID("User");
-            fi.Friend.ID = reader.GetUUID("Friend");
+            fi.User.ID = reader.GetUUID("UserID");
+            fi.Friend.ID = reader.GetUUID("FriendID");
             fi.Secret = (string)reader["Secret"];
             fi.FriendGivenFlags = reader.GetEnum<FriendRightFlags>("RightsToFriend");
             fi.UserGivenFlags = reader.GetEnum<FriendRightFlags>("RightsToUser");
